f78bba65545aa964196c7c5adba517c0e5813608
Shumbusho43 <davidshumbusho10@gmail.com>
2021-07-07 23:13:39 +0200

    added login,updatePassword and photoUpload routes

---
+++ middleware/auth.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const {
+    client
+} = require("../models/database");
+exports.protect = async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.token) {
+        token = req.cookies.token
+    }
+    //verify if token is there
+    if (!token) {
+        return res.status(400).json({
+            message: 'not authorized to access route'
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+            if (err) {
+                err = {
+                    name: 'JsonWebTokenError',
+                    message: 'Token expired. Login again'
+                }
+                console.log(err.message)
+                return res.status(403).json({
+                    message: err.message
+                });
+            } else {
+                client.query(`select clientid,firstname,lastname,gender,email_or_telephone from booking.clients where clientid='${decoded.id}'`, (error, result) => {
+                    if (error) console.log(error);
+                    req.user = result.rows;
+                    next();
+                })
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
